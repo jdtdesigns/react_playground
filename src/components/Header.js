78eb5764c0bdbbe1e6337f9ccd623d51bbd95972
JD Tadlock <jt7903@gmail.com>
2023-05-09 10:31:53 -0400

    styles added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,27 @@
+import '../styles/header.css';
+
+const styles = {
+  header: {
+    padding: '9px 5%',
+    marginBottom: '25px'
+  },
+  h3: {
+    color: '#eee'
+  }
+};
+
 function Header(propsObj) {
   return (
-    <header>
-      <h3>{propsObj.logo} &#128034;</h3>
+    <header style={styles.header}>
+      <h3 style={styles.h3}>{propsObj.logo} &#128034;</h3>
     </header>
   )
 }
 
 export default Header;
+
+// const el = document.querySelector('#some-el');
+
+// el.style.color = 'red';
+
+// el.style.backgroundColor = 'orange';
